02a441134b5d3c5f2aab42bfd919482d48451431
= <=vincentvanmontfoort@gmail.com>
2018-09-11 21:24:33 +0200

    init commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,45 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const fs = require('fs');
+const path = require('path')
+const ext = '.txt'
+const pathName = path.join(__dirname, 'files/')
+
+const operations = {
+    'btnCreate' : 'createFile',
+    'btnRead' : 'readFile',
+    'btnDelete' : 'createFile'
+}
+
+createFile = (file) => {
+    let contents = fileContents.value
+    fs.writeFile(file, contents, (err) => {
+        result(err, 'created')
+    })
+}
+
+readFile = (file) => {
+    fs.readFile(file, (err, data) => {
+        result(err, 'read')
+        fileContents.value = data
+    })
+}
+
+deleteFile = (file) => {
+    fs.unlink(file, (err) => {
+        result(err, 'deleted')
+        fileName.value = ''
+        fileContents.value = ''
+    })
+}
+
+result = (err, operation) => {
+    if(err){
+        return console.log(err)
+    }
+    console.log("The file was succesfully " + operation)
+}
+
+for(o in operations){
+  document.getElementById(o).addEventListener('click', () => {
+    window[operations[o]](pathName + fileName.value + ext)
+  })
+}
